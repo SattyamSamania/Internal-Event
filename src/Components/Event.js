91b5a0179fa2b784d3846a73a17d0e12c056ce86
Sattyam Samania <67833888+SattyamSamania@users.noreply.github.com>
2023-04-26 09:11:20 +0000

    Changes added

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Event(){
   return(
 
     <>
-      
+{/*       
    <div className="container">
   <h1 className="fs-1 text-uppercase text-center m-3 p-3">Add Events</h1>
      <br/>
@@ -34,7 +36,71 @@ export default function Event(){
       <button type="submit" className="btn btn-primary btn-block">Add Event</button>
     </div>
   </form>
-</div>
+</div> */}
+
+
+<section className="upcoming-events">
+  <div className="container">
+    <h2 className="text-center mb-5">Upcoming Events</h2>
+    <div className="row">
+      <div className="col-md-6">
+        <div className="card mb-4">
+          <div className="card-body">
+            <h3 className="card-title">Seminar on Machine Learning</h3>
+            <p className="card-text"><i className="far fa-calendar-alt"></i> Wednesday, May 12, 2023</p>
+            <p className="card-text"><i className="far fa-clock"></i> 6:00 PM - 8:00 PM</p>
+            <p className="card-text"><i className="fas fa-map-marker-alt"></i> 123 Main St, Suite 200, Anytown, USA</p>
+            <a href="#" className="btn btn-primary btn-block">RSVP Now</a>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-6">
+        <div className="card mb-4">
+          <div className="card-body">
+            <h3 className="card-title"> Workshop on React </h3>
+            <p className="card-text"><i className="far fa-calendar-alt"></i> Thursday, June 15, 2023</p>
+            <p className="card-text"><i className="far fa-clock"></i> 9:00 AM - 12:00 PM</p>
+            <p className="card-text"><i className="fas fa-map-marker-alt"></i> 456 Market St, Suite 300, Anytown, USA</p>
+            <a href="#" className="btn btn-primary btn-block">RSVP Now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <section className="upcoming-events">
+  <div className="container">
+    <h2 className="text-center mb-5">Past Events</h2>
+    <div className="row">
+      <div className="col-md-6">
+        <div className="card mb-4">
+          <div className="card-body">
+            <h3 className="card-title">Networking Mixer</h3>
+            <p className="card-text"><i className="far fa-calendar-alt"></i> Wednesday, May 12, 2023</p>
+            <p className="card-text"><i className="far fa-clock"></i> 6:00 PM - 8:00 PM</p>
+            <p className="card-text"><i className="fas fa-map-marker-alt"></i> 123 Main St, Suite 200, Anytown, USA</p>
+            {/* <a href="#" className="btn btn-primary btn-block">RSVP Now</a> */}
+          </div>
+        </div>
+      </div>
+      <div className="col-md-6">
+        <div className="card mb-4">
+          <div className="card-body">
+            <h3 className="card-title">Team Building Workshop</h3>
+            <p className="card-text"><i className="far fa-calendar-alt"></i> Thursday, June 15, 2023</p>
+            <p className="card-text"><i className="far fa-clock"></i> 9:00 AM - 12:00 PM</p>
+            <p className="card-text"><i className="fas fa-map-marker-alt"></i> 456 Market St, Suite 300, Anytown, USA</p>
+            {/* <a href="#" className="btn btn-primary btn-block">RSVP Now</a> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+</section>
+
+
+
  
     </>
   )
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import event1 from './event1.jpg'
+import { Link } from 'react-router-dom';
+import '../App.css'
 
 const Home = () => {
   return(
-    <div>
-     <h1>This is Home</h1>
+    <>
+
+<img className='event1'  src={event1} alt="event1" />
+<section className="home-section">
+  <div className="container">
+    <div className="row">
+      <div className="col-md-6">
+        <h1>Welcome to the Internal Event Management System</h1>
+        <p className="lead">This is the go-to place for managing all your internal events. From scheduling and organizing events to inviting attendees and tracking RSVPs, our system makes event planning a breeze.</p>
+        <div className="row">
+          <div className="col-md-6">
+            <a href="/events" className="btn btn-primary btn-lg btn-block"><i className="fas fa-calendar-alt"></i> View Events</a>
+          </div>
+          <div className="col-md-6">
+            <a href="/create-event" className="btn btn-success btn-lg btn-block"><i className="fas fa-plus-circle"></i> Create Event</a>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-6">
+        <div className="card mb-4">
+          <div className="card-body">
+            <h2 className="card-title"><i className="fas fa-user-friends"></i> Manage Attendees</h2>
+            <p className="card-text">Manage the list of attendees for each event, including their RSVP status and any additional notes.</p>
+            <a href="/manage-attendees" className="btn btn-info"><i className="fas fa-user-friends"></i> Manage Attendees</a>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+</section>
+
+
+    </>
   )
 }
 export default Home;
